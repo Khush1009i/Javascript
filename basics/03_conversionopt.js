6c56087bb62ef6b96cdedd4134a03cfd112fbd16
Khush soni <sonikhush004@gmail.com>
2024-08-28 17:33:02 +0000

    conversion of data types

---
+++ basics/03_conversionopt.js
@@ -0,0 +1,44 @@
+// let score = "Khush"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// number are in lower case in o/p while using score a normal number //
+//let valueInNumber = Number(score)
+
+//console.log( typeof valueInNumber); // NaN as o/p (while using score a normal number)//
+// NaN = not a number ( a special type )//
+//console.log(valueInNumber);
+ // let score = null which gives 0 as o/p         //
+ // let score = undefined which gives NaN as o/p  //
+ /* let score =  true/false { any boolean type } which gives 
+ if true = 1 ; if false = 0(in case when we take score any boolean type value) 
+ */
+// let score = ""{any string} which gives NaN as the o/p //
+/* 
+"33" = 33
+"33abc" = NaN
+*/
+
+let isloggedin = 1
+
+let booleanIsLoggedIn = Boolean(isloggedin)
+
+// console.log( typeof isloggedin)
+
+// console.log(typeof booleanIsLoggedIn);
+//console.log( booleanIsLoggedIn );
+ // 1 == true; 0 ==false //
+ // "" == false //
+ // "khush" == true //
+
+let someNumber = 33
+// console.log( typeof someNumber)
+// console.log( someNumber )
+
+
+let stringNumber = String(someNumber)
+console.log(  stringNumber )
+console.log(  typeof stringNumber )
+ // all we do is investigation //
