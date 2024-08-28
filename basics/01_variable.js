9d03b0e285d071aa93f79d770ed508fb97bb2ca2
Khush soni <sonikhush004@gmail.com>
2024-08-28 11:22:01 +0000

    datatypes

---
+++ basics/01_variable.js
@@ -0,0 +1,27 @@
+"use strict"
+//treat all js code as newer version//
+// alert("hello") // we are using node , not browser
+/*console.log(3 + 3); 
+console.log("Khush") 
+ code redablity is important 
+*/
+let name ="khush"
+let age = 14
+let isloggedin = null
+
+/* number == 2^53 ;
+-bigint
+string== ""
+booloean== true/false
+null == standalone value(representating a null value )
+undefined == the value is not assigned
+symbol == unique
+*/
+
+// object
+// when u assign a vlue to null it will show the object as in its o/p //
+//null is a object //
+
+console.log(typeof null);
+
+console.log(typeof undefined);
