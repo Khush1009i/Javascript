465b1d48b255c68346299eae9635265f6f8eff2d
Khush soni <sonikhush004@gmail.com>
2024-08-29 02:23:28 +0000

    some assignments !

---
+++ assignments/assignment01.js
@@ -0,0 +1,13 @@
+//assignment ( = )
+
+let x = 2 ;
+const y = 3;
+
+console.log(x);
+
+console.log((x = y + 1));
+
+console.log((x = x * y ));
+
+
+
---
+++ basics/04_comparison.js
@@ -0,0 +1,30 @@
+// console.log( 2 > 1 );
+// console.log( 2 >= 1 );
+// console.log( 2 == 1 );
+// console.log( 2 != 1 );
+
+//problem comes when we don't comapre same type of datatype 
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+//problem is like sometime comparison dont give u the predicatable result//
+// so make a confirmation that the both datatype are same // 
+// typescript nevr allows u to comapre differ datatype //
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+// null >=  0 is true becoz there is a problem with value conversion {basically its not predictable} !! //
+// while comaprison js converts null to 0 //
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+ 
+
+//strict check ( " === ")
+
+// console.log( "2" === 2 );
